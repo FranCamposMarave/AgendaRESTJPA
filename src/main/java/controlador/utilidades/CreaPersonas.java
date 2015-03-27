3ff5ea200b80552297d07d59255b5a7eac29afce
Fran <al227761@uji.es>
2015-03-27 16:47:12 +0100

    hola

--- src/main/java/controlador/utilidades/CreaPersonas.java
+++ src/main/java/controlador/utilidades/CreaPersonas.java
@@ -17,7 +17,7 @@ import javax.ws.rs.core.Response;
 @Path("crea")
 @Stateless
 public class CreaPersonas {
-    private GeneradorDatosINE generador = new GeneradorDatosINE();
+    private GeneradorDatosINE generador = new GeneradorDatosINE(); // HOSTIA *PUTA
     @Inject
 //    private BaseDatos  bd;
     private PersonaJPA personaDAO;
